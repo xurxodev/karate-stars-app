ff8e9fd7c56c2d1b05e35f37920ee8763140fc7d
Jorge Sánchez <xurxosf@gmail.com>
2019-12-05 12:30:28 +0100

    Comment widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,15 +4,12 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-/*
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:karate_stars_app/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+/*    // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
@@ -25,7 +22,8 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget);*/
+
   });
 }
-*/
+
