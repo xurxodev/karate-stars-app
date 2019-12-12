8709f52864df276bf21bac8bec6d6b6296e4f2d8
Jorge Sánchez <xurxosf@gmail.com>
2019-12-12 13:18:06 +0100

    Create BlocBase and BlocProvider

---
+++ lib/src/common/presentation/blocs/bloc_provider.dart
@@ -0,0 +1,38 @@
+
+import 'package:flutter/material.dart';
+import 'package:karate_stars_app/src/common/presentation/blocs/bloc_base.dart';
+
+class BlocProvider<T extends BlocBase> extends StatefulWidget {
+  const BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  }): super(key: key);
+
+  final T bloc;
+  final Widget child;
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BlocBase>(BuildContext context){
+    final type = _typeOf<BlocProvider<T>>();
+    final BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
+  }
+
+  static Type _typeOf<T>() => T;
+}
+
+class _BlocProviderState<T> extends State<BlocProvider<BlocBase>>{
+  @override
+  void dispose(){
+    widget.bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return widget.child;
+  }
+}
