d3149cea2f5c9fdec6f490f920607b012f31224a
Jorge Sánchez <xurxosf@gmail.com>
2019-12-17 19:58:02 +0100

    Parse mentions, hashtags and urls in social news texts

--- lib/src/news/presentation/widgets/item_social_news.dart
+++ lib/src/news/presentation/widgets/item_social_news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_parsed_text/flutter_parsed_text.dart';
 import 'package:karate_stars_app/src/common/presentation/widgets/twitter_icon.dart';
 import 'package:karate_stars_app/src/news/domain/entities/social.dart';
 import 'package:karate_stars_app/src/news/presentation/widgets/item_news.dart';
@@ -11,10 +12,12 @@ class ItemSocialNews extends ItemNews {
 
   @override
   Widget buildContent(BuildContext context) {
+    final linkStyle =  TextStyle(color: Theme.of(context).accentColor,);
+
     return Column(children: <Widget>[
       ListTile(
-        leading: CircleAvatar(
-            backgroundImage: NetworkImage(socialNews.user.image)),
+        leading:
+            CircleAvatar(backgroundImage: NetworkImage(socialNews.user.image)),
         title: Text(socialNews.user.name),
         trailing: Text(
           '@${socialNews.user.userName}',
@@ -23,7 +26,33 @@ class ItemSocialNews extends ItemNews {
       ),
       _mediaWidget(),
       const SizedBox(height: 16),
-      ListTile(title: Text(socialNews.summary.title)),
+      ListTile(
+          title: ParsedText(
+        text: socialNews.summary.title,
+        parse: <MatchText>[
+          MatchText(
+            type: ParsedType.URL,
+            style: linkStyle,
+            onTap: (url) {
+              print(url);
+            },
+          ),
+          MatchText(
+            pattern: r'(^|\s)#(\w*[a-zA-Z_]+\S*)',
+            style: linkStyle,
+            onTap: (hashtag) {
+              print(hashtag);
+            },
+          ),
+          MatchText(
+            pattern: '@[A-Za-z0-9]*',
+            style: linkStyle,
+            onTap: (user) {
+              print(user);
+            },
+          ),
+        ],
+      )),
       ListTile(
         leading: TwitterIcon(),
         trailing: Text(
@@ -34,15 +63,15 @@ class ItemSocialNews extends ItemNews {
     ]);
   }
 
-
   Widget _mediaWidget() {
-    if (socialNews.summary.video != null && socialNews.summary.video.isNotEmpty){
+    if (socialNews.summary.video != null &&
+        socialNews.summary.video.isNotEmpty) {
       return ItemVideoPlayer(videoUrl: socialNews.summary.video);
-    } else if (socialNews.summary.image != null && socialNews.summary.image.isNotEmpty){
+    } else if (socialNews.summary.image != null &&
+        socialNews.summary.image.isNotEmpty) {
       return Image.network(socialNews.summary.image);
     } else {
       return Container();
     }
   }
-
 }
