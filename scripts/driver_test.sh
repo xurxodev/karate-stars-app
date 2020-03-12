87e2c5b25f046e377f7fc69c64447d32dc24d973
Jorge Sánchez <xurxosf@gmail.com>
2020-03-12 13:52:09 +0100

    Add codemagic.yaml

---
+++ scripts/driver_test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+flutter drive --target=test_driver/app_empty.dart --driver=test_driver/app_empty_test.dart
+flutter drive --target=test_driver/app_network_error.dart --driver=test_driver/app_network_error_test.dart
