36d60bddf1d5992f5b9f29b37c67fd40b208e728
Jorge Sánchez <xurxosf@gmail.com>
2020-03-21 09:22:01 +0100

    Add test suffix to home news tests

--- scripts/driver_test.sh
+++ scripts/driver_test.sh
@@ -2,4 +2,4 @@
 
 flutter drive --target=test_driver/app_empty.dart --driver=test_driver/app_empty_test.dart
 flutter drive --target=test_driver/app_network_error.dart --driver=test_driver/app_network_error_test.dart
-flutter drive --target=test_driver/app.dart --driver=test_driver/home_news.dart
+flutter drive --target=test_driver/app.dart --driver=test_driver/home_news_test.dart
