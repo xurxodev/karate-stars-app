76edc32706b24d38d8350a4e2fe2332990db3223
Jorge Sánchez <xurxosf@gmail.com>
2020-03-29 12:35:38 +0200

    Try only empty case test

--- scripts/driver_test.sh
+++ scripts/driver_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 flutter drive --target=test_driver/app_empty.dart --driver=test_driver/app_empty_test.dart
-#flutter drive --target=test_driver/app_network_error.dart --driver=test_driver/app_network_error_test.dart
-#flutter drive --target=test_driver/app.dart --driver=test_driver/home_news_test.dart
+flutter drive --target=test_driver/app_network_error.dart --driver=test_driver/app_network_error_test.dart
+flutter drive --target=test_driver/app.dart --driver=test_driver/home_news_test.dart
