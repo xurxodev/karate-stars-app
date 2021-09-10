199525b33db2c5c4b96e4efb04ce60f38c4857a7
Jorge Sánchez <xurxosf@gmail.com>
2021-09-10 08:39:31 +0200

    For the moment, fix device to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:karate_stars_app/app_di.dart' as app_di;
 import 'package:karate_stars_app/src/app.dart';
 
@@ -6,6 +7,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await app_di.init();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
 
   runApp(App());
 }
