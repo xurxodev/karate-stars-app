8296416b3defeb4a46c0caae86e4a63886a8323d
Jorge Sánchez <xurxosf@gmail.com>
2022-02-08 20:06:12 +0100

    Fix analyze

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
