8060d2ccb6af478a434a2f1f442b3e0dcbf30112
Jorge Sánchez <xurxosf@gmail.com>
2022-02-08 19:21:00 +0100

    Remove asset_does_not_exist warning for .env

--- lib/src/news/presentation/widgets/news_app_bar.dart
+++ lib/src/news/presentation/widgets/news_app_bar.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:karate_stars_app/src/common/keys.dart';
 import 'package:karate_stars_app/src/common/presentation/blocs/bloc_provider.dart';
 import 'package:karate_stars_app/src/common/presentation/functions/showPlatformDialog.dart';
-import 'package:karate_stars_app/src/common/presentation/functions/url.dart'
-    as url_helper;
 import 'package:karate_stars_app/src/common/presentation/widgets/actions/FilterAction.dart';
 import 'package:karate_stars_app/src/common/presentation/widgets/platform/platform_alert_dialog.dart';
 import 'package:karate_stars_app/src/common/presentation/widgets/platform/platform_menu.dart';
@@ -12,7 +9,6 @@ import 'package:karate_stars_app/src/common/strings.dart';
 import 'package:karate_stars_app/src/news/presentation/blocs/news_bloc.dart';
 import 'package:karate_stars_app/src/news/presentation/widgets/news_filter.dart';
 import 'package:karate_stars_app/src/search/presentation/page/search_page.dart';
-import 'package:karate_stars_app/src/settings/presentation/page/settings_page.dart';
 
 class NewsAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
