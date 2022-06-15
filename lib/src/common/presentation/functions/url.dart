a309f3e34d8fc5d38b1ec04e28bb6a5d9e431106
xurxodev <xurxosf@gmail.com>
2022-06-15 16:57:21 +0200

    Send url to visit as screenview

--- lib/src/common/presentation/functions/url.dart
+++ lib/src/common/presentation/functions/url.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:karate_stars_app/app_di.dart' as app_di;
+import 'package:karate_stars_app/src/common/presentation/boundaries/analytics.dart';
 import 'package:karate_stars_app/src/rate_app/presentation/utils.dart';
 
 Future<void> launchURL(BuildContext context, String url) async {
 
+  final analyticsService = app_di.getIt<AnalyticsService>();
+
+  analyticsService.sendScreenName('url:$url');
 
   final browser = ChromeSafariBrowser();
 
